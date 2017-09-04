82af01e3da0f254c4860fb6b254724a2bb14391b
pasha-kuznetsov <pasha@tydbits.com>
2017-09-04 12:09:51 -0500

    L3 3 done

--- src/main/java/com/tydbits/google/challenges/L3_3_queue_to_do/Answer.java
+++ src/main/java/com/tydbits/google/challenges/L3_3_queue_to_do/Answer.java
@@ -2,7 +2,13 @@ package com.tydbits.google.challenges.L3_3_queue_to_do;
 
 public class Answer {
     public static int answer(int start, int length) {
-        // Your code goes here.
-        return 0;
+        int checksum = 0;
+        for (int line = 0; line < length; ++line) {
+            for (int w = 0; w < length - line; ++w) {
+                int worker = start + line * length + w;
+                checksum = checksum ^ worker;
+            }
+        }
+        return checksum;
     }
 }
