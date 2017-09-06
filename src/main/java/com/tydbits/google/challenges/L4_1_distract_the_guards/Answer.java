f675ca3f96196b74b51e609e7d4e79e3c7d4e18e
pasha-kuznetsov <pasha@tydbits.com>
2017-09-06 00:19:50 -0500

    Level 4 challenge 1 done

--- src/main/java/com/tydbits/google/challenges/L4_1_distract_the_guards/Answer.java
+++ src/main/java/com/tydbits/google/challenges/L4_1_distract_the_guards/Answer.java
@@ -1,133 +1,141 @@
 package com.tydbits.google.challenges.L4_1_distract_the_guards;
 
-import java.util.Arrays;
-import java.util.BitSet;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 
+// everything has to be in one class for Google FooBar
 public class Answer {
-    private static final HashSet<Pair> finite = new HashSet<>();
-    private static final HashSet<Pair> infinite = new HashSet<>();
-
     public static int answer(int[] banana_list) {
-        return new Query(banana_list).minFinite();
+        return banana_list.length - new Query(banana_list).maxMatching();
     }
 
     static class Query {
-        private final int[] bananas;
-        private final HashMap<String /* bananas */, BitSet /* pairing */> bestPairings;
+        final Guard[] guards;
 
         Query(int[] bananas) {
-            this.bananas = bananas;
-            this.bestPairings = new HashMap<>();
-            Arrays.sort(bananas);
+            this.guards = new GuardsData(bananas).sortGuards(new GuardPairsSizeCmp());
         }
 
-        int minFinite() {
-            BitSet bestPairing = bestPairing(new BitSet(bananas.length), 0);
-            return bananas.length - bestPairing.cardinality();
-        }
-
-        private BitSet bestPairing(BitSet mask, int start) {
-            StringBuilder sb = new StringBuilder();
-            for (int i = mask.nextClearBit(0); i < bananas.length; i = mask.nextClearBit(++i)) {
-                if (sb.length() > 0) sb.append(',');
-                sb.append(bananas[i]);
+        // sort by number of connections, trying to minimize freeUp calls
+        private class GuardPairsSizeCmp implements Comparator<Guard> {
+            @Override
+            public int compare(Guard o1, Guard o2) {
+                return Integer.compare(o1.matches.size(), o2.matches.size());
             }
-            String key = sb.toString();
-            BitSet pairing = bestPairings.get(key);
-            if (pairing == null)
-                bestPairings.put(key, pairing = newBestPairing(mask, start));
-            return pairing;
         }
 
-        private BitSet newBestPairing(BitSet mask, int start) {
-            BitSet bestPairing = mask;
-            for (int i = mask.nextClearBit(start); i < bananas.length - 1; i = mask.nextClearBit(++i)) {
-                for (int j = mask.nextClearBit(i + 1); j < bananas.length; j = mask.nextClearBit(++j)) {
-                    if (!isFiniteMatch(new Pair(bananas[i], bananas[j]))) {
-                        BitSet pairing = (BitSet) bestPairing(markPaired(mask, i, j), i + 1).clone();
-                        pairing.clear(0, i);
-                        pairing.or(mask);
-                        int cardinality = pairing.cardinality();
-                        if (cardinality >= bananas.length)
-                            return pairing;
-                        if (cardinality > bestPairing.cardinality())
-                            bestPairing = pairing;
-                    }
+        int maxMatching() {
+            for (Guard guard : guards) {
+                if (guard.pair == null)
+                    new MatchMaker().pairUp(guard);
+            }
+            return countPaired();
+        }
+
+        private int countPaired() {
+            int paired = 0;
+            for (Guard guard : guards) {
+                if (guard.pair != null)
+                    paired = paired + 1;
+            }
+            return paired;
+        }
+    }
+
+    private static class MatchMaker {
+        private HashSet<Guard> seen = new HashSet<>();
+
+        boolean pairUp(Guard guard) {
+            if (!seen.add(guard)) return false;
+            Guard pair = findFreePair(guard);
+            if (pair == null) pair = freeUpPair(guard);
+            if (pair == null) return false;
+            guard.pair = pair;
+            pair.pair = guard;
+            return true;
+        }
+
+        private Guard findFreePair(Guard guard) {
+            for (Guard pair : guard.matches) {
+                if (pair.pair != null) continue;
+                if (!seen.add(pair)) continue;
+                return pair;
+            }
+            return null;
+        }
+
+        private Guard freeUpPair(Guard guard) {
+            for (Guard pair : guard.matches) {
+                if (!seen.add(pair)) continue;
+                if (!pairUp(pair.pair)) continue;
+                pair.pair = null;
+                return pair;
+            }
+            return null;
+        }
+    }
+
+    static class Guard {
+        ArrayList<Guard> matches = new ArrayList<>();
+        Guard pair;
+    }
+
+    static class Match {
+        private final int a;
+        private final int b;
+
+        Match(int a, int b) {
+            this.a = Math.max(a, b);
+            this.b = Math.min(a, b);
+        }
+
+        static boolean isInfinite(Match m) {
+            for (; ; ) {
+                if (m.a == m.b || m.a <= 0 && m.b <= 0) return false;
+                if ((m.a + m.b) % 2 == 1) return true;
+                int gcd = gcd(m.a, m.b);
+                m = new Match((m.a - m.b) / gcd, m.b / gcd * 2);
+            }
+        }
+
+        private static int gcd(int a, int b) {
+            while (b > 0) {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+    }
+
+    private static class GuardsData {
+        private HashMap<Integer, Guard> map = new HashMap<>();
+
+        GuardsData(int[] bananas) {
+            for (int i = 0; i < bananas.length - 1; i++) {
+                for (int j = i + 1; j < bananas.length; j++) {
+                    if (Match.isInfinite(new Match(bananas[i], bananas[j])))
+                        connectGuards(guard(i), guard(j));
                 }
             }
-            return bestPairing;
         }
 
-        private BitSet markPaired(BitSet mask, int i, int j) {
-            BitSet m = (BitSet) mask.clone();
-            m.set(0, i + 1);
-            m.set(j);
-            return m;
-        }
-    }
-
-    static boolean isFiniteMatch(Pair pair) {
-        HashSet<Pair> seen = new HashSet<>();
-        for (; canMatch(pair) && !finite.contains(pair); pair = match(pair)) {
-            if (infinite.contains(pair) || !seen.add(pair)) {
-                infinite.addAll(seen);
-                return false;
-            }
-        }
-        finite.addAll(seen);
-        return true;
-    }
-
-    private static boolean canMatch(Pair pair) {
-        return pair.larger != pair.smaller && pair.larger > 0 && pair.smaller > 0;
-    }
-
-    private static Pair match(Pair pair) {
-        return new Pair(pair.smaller * 2, pair.larger - pair.smaller);
-    }
-
-    static class Pair implements Comparable<Pair> {
-        private final int smaller;
-        private final int larger;
-
-        Pair(int a, int b) {
-            if (a > b){
-                int d = gcd(b, a);
-                this.larger = a / d;
-                this.smaller = b / d;
-            } else {
-                int d = gcd(a, b);
-                this.larger = b / d;
-                this.smaller = a / d;
-            }
+        private void connectGuards(Guard guard1, Guard guard2) {
+            guard1.matches.add(guard2);
+            guard2.matches.add(guard1);
         }
 
-        private static int gcd(int larger, int smaller) {
-            return smaller == 0 ? larger : gcd(smaller, larger % smaller);
+        private Guard guard(int id) {
+            Guard guard = map.get(id);
+            if (guard == null) map.put(id, guard = new Guard());
+            return guard;
         }
 
-        @Override
-        public boolean equals(Object obj) {
-            return obj instanceof Pair && (obj == this || compareTo((Pair) obj) == 0);
-        }
-
-        @Override
-        public int compareTo(Pair o) {
-            int aCmp = Integer.compare(smaller, o.smaller);
-            if (aCmp != 0) return aCmp;
-            return Integer.compare(larger, o.larger);
-        }
-
-        @Override
-        public int hashCode() {
-            return new Long(((long) smaller << 32) | larger).hashCode();
-        }
-
-        @Override
-        public String toString() {
-            return "(" + smaller + ", " + larger + ")";
+        Guard[] sortGuards(Comparator<Guard> cmp) {
+            Guard[] guards = map.values().toArray(new Guard[map.size()]);
+            Arrays.sort(guards, cmp);
+            for (Guard guard : guards)
+                Collections.sort(guard.matches, cmp);
+            return guards;
         }
     }
 }
