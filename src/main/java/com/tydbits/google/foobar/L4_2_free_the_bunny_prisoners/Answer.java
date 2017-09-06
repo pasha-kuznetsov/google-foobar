2cb2a160bd502d847fea56c7c83ba6d266a36fdc
pasha-kuznetsov <pasha@tydbits.com>
2017-09-06 00:44:02 -0500

    L4 challenge 2 "Free the Bunny Prisoners"

---
+++ src/main/java/com/tydbits/google/foobar/L4_2_free_the_bunny_prisoners/Answer.java
@@ -0,0 +1,8 @@
+package com.tydbits.google.foobar.L4_2_free_the_bunny_prisoners;
+
+public class Answer {
+    public static int[][] answer(int num_buns, int num_required) {
+        // Your code goes here.
+        return new int[0][];
+    }
+}
---
+++ src/test/groovy/com/tydbits/google/foobar/L4_2_free_the_bunny_prisoners/L4_2_AnswerTest.groovy
@@ -0,0 +1,24 @@
+package com.tydbits.google.foobar.L4_2_free_the_bunny_prisoners
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class L4_2_AnswerTest extends Specification {
+    def answer = new Answer()
+
+    @Unroll
+    def "test #num_buns #num_required"() {
+        expect:
+        answer.answer(buns, required) == expectedAnswer as int[][]
+
+        where:
+        buns | required || expectedAnswer
+        2    | 1        || [[0], [0]]
+        5    | 3        || [[0, 1, 2, 3, 4, 5],
+                            [0, 1, 2, 6, 7, 8],
+                            [0, 3, 4, 6, 7, 9],
+                            [1, 3, 5, 6, 8, 9],
+                            [2, 4, 5, 7, 8, 9]]
+        4    | 4        || [[0], [1], [2], [3]]
+    }
+}
