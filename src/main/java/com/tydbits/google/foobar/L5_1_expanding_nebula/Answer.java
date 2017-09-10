674d9e19a817a68bc525636b142704392d6e8f5d
pasha-kuznetsov <pasha@tydbits.com>
2017-09-10 02:28:39 -0500

    L5 1 cleanup

--- src/main/java/com/tydbits/google/foobar/L5_1_expanding_nebula/Answer.java
+++ src/main/java/com/tydbits/google/foobar/L5_1_expanding_nebula/Answer.java
@@ -33,14 +33,6 @@ public class Answer {
             this.columns = new ArrayList<>();
             for (int column = 0; column < cells[0].length; ++column)
                 columns.add(new ColumnBuilder(column).generateColumns());
-
-            boolean restricted;
-            do {
-                restricted = false;
-                for (int column = 1; column < columns.size(); ++column)
-                    if (restrict(columns.get(column - 1), columns.get(column)))
-                        restricted = true;
-            } while (restricted);
         }
 
         int count() {
@@ -98,8 +90,6 @@ public class Answer {
             }
 
             private void generateColumns(Cell cell, Pattern pattern) {
-                if (!isFirstColumn(cell) && !leftFrom(cell).validRight(pattern)) return;
-                if (!isLastColumn(cell) && !rightFrom(cell).validLeft(pattern)) return;
                 setLeft(cell.row, pattern.left);
                 setRight(cell.row, pattern.right);
                 if (isLastRow(cell)) {
@@ -110,6 +100,10 @@ public class Answer {
                     generateColumns(next, next.topPatterns.get(pattern.bottom));
                 }
             }
+
+            private boolean isLastRow(Cell cell) { return cell.row >= cells.length - 1; }
+
+            private Cell nextRow(Cell cell) { return cells[cell.row + 1][cell.column]; }
 
             void setLeft(int row, int value) {
                 left &= ~(0b11 << (row * 2));
@@ -126,98 +120,50 @@ public class Answer {
             }
         }
 
-        private boolean restrict(Columns left, Columns right) {
-            HashSet<Integer> common = intersect(left.right.keySet(), right.left.keySet());
-            if (common.size() == left.right.size() && common.size() == right.left.size())
-                return false;
-            left.removeRight(diff(left.right.keySet(), common));
-            right.removeLeft(diff(right.left.keySet(), common));
-            return true;
+        static class Columns {
+            private final int index;
+            final Multimap<Integer, Column> left;
+            final Multimap<Integer, Column> right;
+
+            Columns(int column) {
+                this.index = column;
+                this.left = new Multimap<>();
+                this.right = new Multimap<>();
+            }
+
+            void add(Column column) {
+                left.addValue(column.left, column);
+                right.addValue(column.right, column);
+            }
         }
 
-        private boolean isFirstColumn(Cell cell) { return cell.column <= 0; }
-        private boolean isLastColumn(Cell cell) { return cell.column + 1 >= cells[cell.row].length; }
-        private boolean isLastRow(Cell cell) { return cell.row >= cells.length - 1; }
-        private Cell leftFrom(Cell cell) { return cells[cell.row][cell.column - 1]; }
-        private Cell rightFrom(Cell cell) { return cells[cell.row][cell.column + 1]; }
-        private Cell nextRow(Cell cell) { return cells[cell.row + 1][cell.column]; }
-    }
+        static class Column {
+            int left;
+            int right;
+            int count = -1;
 
-    static class Columns {
-        private final int index;
-        final Multimap<Integer, Column> left;
-        final Multimap<Integer, Column> right;
-
-        Columns(int column) {
-            this.index = column;
-            this.left = new Multimap<>();
-            this.right = new Multimap<>();
+            Column(int left, int right) {
+                this.left = left;
+                this.right = right;
+            }
         }
 
-        void add(Column column) {
-            left.addValue(column.left, column);
-            right.addValue(column.right, column);
-        }
+        static class Cell {
+            int row;
+            int column;
+            final HashSet<Pattern> patterns;
+            final Multimap<Integer, Pattern> topPatterns;
 
-        void removeLeft(Set<Integer> remove) {
-            for (Integer key : remove) {
-                for (Column c : left.get(key)) {
-                    right.removeValue(c.right, c);
+            Cell(int row, int column, ArrayList<Pattern> patterns) {
+                this.row = row;
+                this.column = column;
+                this.patterns = new HashSet<>();
+                this.topPatterns = new Multimap<>();
+                for (Pattern pattern : patterns) {
+                    this.patterns.add(pattern);
+                    topPatterns.addValue(pattern.top, pattern);
                 }
             }
-            left.keySet().removeAll(remove);
-        }
-
-        void removeRight(Set<Integer> remove) {
-            for (Integer key : remove) {
-                for (Column c : right.get(key)) {
-                    left.removeValue(c.left, c);
-                }
-            }
-            right.keySet().removeAll(remove);
-        }
-    }
-
-    static class Column {
-        int left;
-        int right;
-        int count = -1;
-
-        Column(int left, int right) {
-            this.left = left;
-            this.right = right;
-        }
-    }
-
-    static class Cell {
-        int row;
-        int column;
-        final HashSet<Pattern> patterns;
-        final Multimap<Integer, Pattern> topPatterns;
-        final Multimap<Integer, Pattern> leftPatterns;
-        final Multimap<Integer, Pattern> rightPatterns;
-
-        Cell(int row, int column, ArrayList<Pattern> patterns) {
-            this.row = row;
-            this.column = column;
-            this.patterns = new HashSet<>();
-            this.topPatterns = new Multimap<>();
-            this.leftPatterns = new Multimap<>();
-            this.rightPatterns = new Multimap<>();
-            for (Pattern pattern : patterns) {
-                this.patterns.add(pattern);
-                topPatterns.addValue(pattern.top, pattern);
-                leftPatterns.addValue(pattern.left, pattern);
-                rightPatterns.addValue(pattern.right, pattern);
-            }
-        }
-
-        boolean validLeft(Pattern pattern) {
-            return leftPatterns.containsKey(pattern.right);
-        }
-
-        boolean validRight(Pattern pattern) {
-            return rightPatterns.containsKey(pattern.left);
         }
     }
 
@@ -235,41 +181,13 @@ public class Answer {
             this.right = ((top & 0b01) << 1) | ((bottom & 0b01));
             this.count = (top & 0b01) + ((top & 0b10) >> 1) + (bottom & 0b01) + ((bottom & 0b10) >> 1);
         }
-
-        @Override
-        public String toString() {
-            return ((top & 0b10) == 0 ? "." : "o") + ((top & 0b01) == 0 ? '.' : 'o') + ' ' +
-                    ((bottom & 0b10) == 0 ? "." : "o") + ((bottom & 0b01) == 0 ? '.' : 'o');
-        }
     }
 
     static class Multimap<K, V> extends HashMap<K, HashSet<V>> {
-
         void addValue(K key, V value) {
             HashSet<V> set = get(key);
             if (set == null) super.put(key, set = new HashSet<>());
             set.add(value);
         }
-
-        void removeValue(K key, V value) {
-            HashSet<V> set = get(key);
-            if (set == null)
-                return;
-            set.remove(value);
-            if (set.isEmpty())
-                remove(key);
-        }
-    }
-
-    static <T> HashSet<T> intersect(Set<T> left, Set<T> right) {
-        HashSet<T> common = new HashSet<>(left);
-        common.retainAll(right);
-        return common;
-    }
-
-    static <T> HashSet<T> diff(Set<T> set, Set<T> remove) {
-        HashSet<T> diff = new HashSet<>(set);
-        diff.removeAll(remove);
-        return diff;
     }
 }
