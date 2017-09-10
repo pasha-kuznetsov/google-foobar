bc3f6b1db250fabadd55a4d7d9a36ea762ec1931
pasha-kuznetsov <pasha@tydbits.com>
2017-09-09 23:34:21 -0500

    L5 1 optimizaton: generate rows instead of columns

--- src/main/java/com/tydbits/google/foobar/L5_1_expanding_nebula/Answer.java
+++ src/main/java/com/tydbits/google/foobar/L5_1_expanding_nebula/Answer.java
@@ -1,8 +1,6 @@
 package com.tydbits.google.foobar.L5_1_expanding_nebula;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 
 public class Answer {
     public static int answer(boolean[][] g) {
@@ -13,13 +11,13 @@ public class Answer {
         private final ArrayList<Pattern> gasPatterns;
         private final ArrayList<Pattern> emptyPatterns;
         private final Cell[][] cells;
-        private final ArrayList<Columns> columns;
+        private final ArrayList<Rows> rows;
         private int terminal;
 
         Query(boolean[][] g) {
             this.gasPatterns = new ArrayList<>();
             this.emptyPatterns = new ArrayList<>();
-            for (String top : new String[] {"..", ".o", "o.", "oo"}) {
+            for (String top : new String[]{"..", ".o", "o.", "oo"}) {
                 for (String bottom : new String[]{"..", ".o", "o.", "oo"}) {
                     // empty cell -> 1 non-empty predecessor
                     // non-empty -> other number of non-empty predecessors
@@ -35,103 +33,171 @@ public class Answer {
                 }
             }
 
-            this.columns = new ArrayList<>();
-            for (int j = 0; j < cells[0].length; ++j)
-                columns.add(generateColumns(new Columns(j), "", "", null, cells[0][j]));
+            this.rows = new ArrayList<>();
+            for (int row = 0; row < cells.length; ++row)
+                rows.add(generateRows(new Rows(row), "", "", null, cells[row][0]));
+
+            boolean restricted;
+            do {
+                restricted = false;
+                for (int row = 1; row < rows.size(); ++row)
+                    if (restrict(rows.get(row - 1), rows.get(row)))
+                        restricted = true;
+            } while (restricted);
         }
 
-        private Columns generateColumns(Columns columns, String left, String right, Pattern top, Cell cell) {
-            for (Pattern pattern : cell.patterns) {
-                if (top != null && !pattern.top.equals(top.bottom))
-                    continue;
-                if (cell.row == cells.length - 1) {
-                    Column column = new Column(left + pattern.left, right + pattern.right);
-                    columns.add(column);
-                } else {
-                    generateColumns(columns,
-                            left + pattern.left.charAt(0),
-                            right + pattern.right.charAt(0),
-                            pattern,
-                            cells[cell.row + 1][cell.column]);
+        private Rows generateRows(Rows rows, String top, String bottom, Pattern left, Cell cell) {
+            HashSet<Pattern> patterns = left == null ? cell.patterns : cell.patternsByLeft.get(left.right);
+            if (patterns != null) {
+                for (Pattern pattern : patterns) {
+                    generateRows(rows, top, bottom, cell, pattern);
                 }
             }
-            return columns;
+            return rows;
+        }
+
+        private void generateRows(Rows rows, String top, String bottom, Cell cell, Pattern pattern) {
+            if (cell.column == cells[0].length - 1)
+                rows.add(new Row(top + pattern.top, bottom + pattern.bottom));
+            else
+                generateRows(rows,
+                        top + pattern.top.charAt(0),
+                        bottom + pattern.bottom.charAt(0),
+                        pattern,
+                        cells[cell.row][cell.column + 1]);
+        }
+
+        private boolean restrict(Rows left, Rows right) {
+            HashSet<String> common = intersect(left.bottom.keySet(), right.top.keySet());
+            if (common.size() == left.bottom.size() && common.size() == right.top.size())
+                return false;
+            left.removeBottom(diff(left.bottom.keySet(), common));
+            right.removeTop(diff(right.top.keySet(), common));
+            return true;
         }
 
         int count() {
             this.terminal = 0;
-            countColumns(columns.get(0));
+            countRows(rows.get(0));
             return terminal;
         }
 
-        private void countColumns(Columns c) {
-            for (HashSet<Column> right : c.right.values()) {
-                for (Column column : right) {
-                    countColumns(c, column);
+        private void countRows(Rows rows) {
+            for (HashSet<Row> right : rows.bottom.values()) {
+                for (Row row : right) {
+                    countRows(rows, row);
                 }
             }
         }
 
-        private void countColumns(Columns c, Column column) {
-            if (c.column == columns.size() - 1) {
+        private void countRows(Rows c, Row row) {
+            if (c.row == rows.size() - 1)
                 terminal += 1;
-            }
             else
-                countColumns(columns.get(c.column + 1), column.right);
+                countRows(rows.get(c.row + 1), row.bottom);
         }
 
-        private void countColumns(Columns c, String leftKey) {
-            HashSet<Column> set = c.left.get(leftKey);
+        private void countRows(Rows c, String top) {
+            HashSet<Row> set = c.top.get(top);
             if (set == null)
                 return;
-            for (Column column : set)
-                countColumns(c, column);
+            for (Row row : set)
+                countRows(c, row);
         }
 
-        static class Columns {
-            private final int column;
-            final HashMap<String, HashSet<Column>> left;
-            final HashMap<String, HashSet<Column>> right;
+        static class Rows {
+            private final int row;
+            final Multimap<String, Row> top;
+            final Multimap<String, Row> bottom;
 
-            Columns(int column) {
-                this.column = column;
-                this.left = new HashMap<>();
-                this.right = new HashMap<>();
+            Rows(int row) {
+                this.row = row;
+                this.top = new Multimap<>();
+                this.bottom = new Multimap<>();
             }
 
-            public void add(Column column) {
-                add(left, column.left, column);
-                add(right, column.right, column);
+            void add(Row row) {
+                top.addValue(row.top, row);
+                bottom.addValue(row.bottom, row);
             }
 
-            private void add(HashMap<String, HashSet<Column>> map, String key, Column column) {
-                HashSet<Column> set = map.get(key);
-                if (set == null) map.put(key, set = new HashSet<>());
-                set.add(column);
+            void removeTop(Set<String> remove) {
+                for (String key : remove) {
+                    for (Row c : top.get(key)) {
+                        bottom.removeValue(c.bottom, c);
+                    }
+                }
+                top.keySet().removeAll(remove);
+            }
+
+            void removeBottom(Set<String> remove) {
+                for (String key : remove) {
+                    for (Row c : bottom.get(key)) {
+                        top.removeValue(c.top, c);
+                    }
+                }
+                bottom.keySet().removeAll(remove);
             }
         }
 
-        static class Column {
-            String left = "";
-            String right = "";
+        static class Row {
+            String top = "";
+            String bottom = "";
 
-            Column(String left, String right) {
-                this.left = left;
-                this.right = right;
+            Row(String top, String bottom) {
+                this.top = top;
+                this.bottom = bottom;
             }
         }
 
         static class Cell {
             int row;
             int column;
-            private ArrayList<Pattern>/*TODO Set<Pattern>*/ patterns;
+            final HashSet<Pattern> patterns;
+            final Multimap<String, Pattern> patternsByLeft;
 
             Cell(int row, int column, ArrayList<Pattern> patterns) {
                 this.row = row;
                 this.column = column;
-                this.patterns = new ArrayList<>(patterns);
+                this.patterns = new HashSet<>();
+                this.patternsByLeft = new Multimap<>();
+                for (Pattern pattern : patterns) {
+                    this.patterns.add(pattern);
+                    patternsByLeft.addValue(pattern.left, pattern);
+                }
             }
+
         }
+    }
+
+    static class Multimap<K, V> extends HashMap<K, HashSet<V>> {
+
+        void addValue(K key, V value) {
+            HashSet<V> set = get(key);
+            if (set == null) super.put(key, set = new HashSet<>());
+            set.add(value);
+        }
+
+        void removeValue(K key, V value) {
+            HashSet<V> set = get(key);
+            if (set == null)
+                return;
+            set.remove(value);
+            if (set.isEmpty())
+                remove(key);
+        }
+    }
+
+    static HashSet<String> intersect(Set<String> left, Set<String> right) {
+        HashSet<String> common = new HashSet<>(left);
+        common.retainAll(right);
+        return common;
+    }
+
+    static HashSet<String> diff(Set<String> set, Set<String> remove) {
+        HashSet<String> diff = new HashSet<>(set);
+        diff.removeAll(remove);
+        return diff;
     }
 
     static class Pattern {
